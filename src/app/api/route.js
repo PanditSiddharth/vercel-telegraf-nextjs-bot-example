fa34288b2b38e69d718de5bd63366053b2b8aa93
Siddharth sharma <mynmss03@gmail.com>
2024-08-21 12:12:05 +0530

    initial

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -11,7 +11,7 @@ export async function POST(request) {
 
 export async function GET() {
     try {
-        let url = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/setWebhook?url=${process.env.VERCEL_URL}`
+        let url = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/setWebhook?url=${process.env.VERCEL_URL}/api`
         let res = await fetch(url)
             .then(res => res.json())
         return NextResponse.json(res);
